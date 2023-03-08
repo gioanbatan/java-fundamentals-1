5c31c2f5f9cae78c2b00be095ba456335f685c0c
gioanbatan <giovannibassani@hotmail.com>
2023-03-08 14:41:01 +0100

    first commit

---
+++ src/org/lessons/java/TrainBill.java
@@ -0,0 +1,12 @@
+package org.lessons.java;
+
+public class TrainBill {
+    public static void main(String[] args) {
+        int tripDistance = 0;
+        int userAge = 0;
+
+        System.out.println("Ciao!");
+        System.out.print("Per favore, inserisci il numero di chilometri che vuoi percorrere: ");
+
+    }
+}
