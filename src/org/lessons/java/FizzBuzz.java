f275efb5c36e1d947d4e824374a160c13fa9296a
gioanbatan <giovannibassani@hotmail.com>
2023-03-08 17:57:29 +0100

    Fix: exercise 2 + bonus

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,11 +1,26 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
         String fizzBuzzText ="";
+        int numbersQty;
 
-        // Ciclo for che scorra da numero a numero
-        for (int i = 1; i < 101; i++) {
+        // Richiesta della quantità di numeri;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("Inserisci un numero maggiore di 0 e minore di 1000: ");
+
+            numbersQty = Integer.parseInt(scan.nextLine());
+        } while (numbersQty <= 0 || numbersQty >= 1000);
+
+        // Ciclo for che scorre i numeri
+        for (int i = 1; i <= numbersQty; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                System.out.print(i + " ");
+            }
             if (i % 3 == 0) {
                 fizzBuzzText += "Fizz ";
             }
@@ -13,12 +28,8 @@ public class FizzBuzz {
                 fizzBuzzText += "Buzz ";
             }
 
-            if (fizzBuzzText != "") {
-                System.out.print(fizzBuzzText);
-            } else {
-                System.out.print(i + " ");
-            }
-
+            System.out.print(fizzBuzzText);
+            // Svuotamento stringa
             fizzBuzzText = "";
         }
     }
